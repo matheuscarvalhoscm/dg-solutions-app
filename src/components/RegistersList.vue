1bb7d492b769588980408370142dc56d76db1242
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-07 16:10:20 -0300

    feat: create `RegisterList` component

---
+++ src/components/RegistersList.vue
@@ -0,0 +1,23 @@
+<template>
+  <table class="register-table">
+    <tr>
+      <th>Nome</th>
+      <th>Idade</th>
+    </tr>
+    <tr v-for="register in registerList" :key="register.id">
+      <td>{{ register.name }}</td>
+      <td>{{ register.birthDate }}</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'RegistersList',
+  data() {
+    return {
+      registerList: [],
+    };
+  },
+};
+</script>
