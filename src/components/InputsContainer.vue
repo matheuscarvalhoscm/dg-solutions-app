201979d4940a74100fdbd355b41b312b5b3de11b
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-07 16:10:04 -0300

    feat: create `InputsContainer` component

---
+++ src/components/InputsContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="inputs-container">
+    <h1>Formulário de inscrição</h1>
+    <form>
+      <label for="name-input">Nome completo</label>
+      <input type="text" name="name-input" v-model="name" />
+      <label for="birthdate-input">Data de Nascimento</label>
+      <input type="date" name="birthdate-input" v-model="birthdate" />
+    </form>
+  </div>
+</template>
+  
+<script>
+  export default {
+    name: 'InputsContainer',
+    data() {
+      return {
+        name: '',
+        birthdate: '',
+      }
+    }
+  }
+</script>
