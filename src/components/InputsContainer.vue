bd5ac96334f12db27f18310521a5d2745085e7ae
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-08 14:22:58 -0300

    style: add `InputsContainer` component style

--- src/components/InputsContainer.vue
+++ src/components/InputsContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="inputs-container">
-    <h1>Formulário de inscrição</h1>
+    <h1>DG Solutions Challenge</h1>
     <form>
       <label for="name-input">Nome completo</label>
       <input type="text" name="name-input" v-model="name" />
@@ -29,3 +29,54 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.inputs-container {
+  padding: 1em;
+  border-radius: 10px;
+  box-shadow: 1px 1px 4px rgb(199, 199, 199);
+  margin-left: 2em;
+}
+
+label,
+input {
+  display: block;
+}
+
+label {
+  margin: 0.7em 0;
+}
+
+input {
+  padding: 0.5em;
+  border-style: none;
+  border: 3px solid rgb(228, 228, 248);
+  border-radius: 5px;
+  transition: 0.5s;
+}
+
+input:focus {
+  border: 3px solid rgb(76, 76, 168);
+  outline: none;
+}
+
+input[type="text"] {
+  width: 80%;
+}
+
+button {
+  background-color: rgb(89, 89, 214);
+  border-radius: 5px;
+  border: none;
+  color:  rgb(241, 241, 248);
+  cursor: pointer;
+  margin-top: 1em;
+  padding: 1em;
+  transition: 0.5s;
+}
+
+button:hover {
+  background-color: rgb(15, 15, 114);
+}
+
+</style>
